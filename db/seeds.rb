53637265b6df7c3466ba70172190d291d2c6e1b3
github username <github email address>
2018-09-07 14:47:38 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+@rick_and_morty = Show.create(:name => "Rick and Morty", :day => "Thursday", :network => "Adult Swim", :rating => 10)
+@law_and_order = Show.create(:name => "Law & Order", :day => "Monday", :network => "NBC", :rating => 7)
